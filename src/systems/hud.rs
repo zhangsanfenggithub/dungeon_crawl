9ed961276a73dbdfffa0b13725c7842ebccd7231
zhangchuanzhi <zhang_chuanzhi@outlook.com>
2024-07-21 23:18:23 +0800

    update:更新血条ui和显示

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -1,11 +1,10 @@
 use crate::prelude::*;
 
 
-
 #[system]
 #[read_component(Health)]
 #[read_component(Player)]
-pub fn hud(ecs : &mut SubWorld){
+pub fn hud(ecs: &mut SubWorld) {
     let mut health_query = <&Health>::query().filter(component::<Player>());
     let player_health = health_query
         .iter(ecs)
@@ -15,5 +14,10 @@ pub fn hud(ecs : &mut SubWorld){
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
     draw_batch.print_centered(1, "Explore the dungeon. Cursor keys to move.");
+
+    draw_batch.bar_horizontal(Point::zero(), SCREEN_WIDTH * 2, player_health.current, player_health.max, ColorPair::new(RED, RED));
+    draw_batch.print_color_centered(0, format!("Health : {} / {} ", player_health.current, player_health.max), ColorPair::new(WHITE, RED));
+
     draw_batch.submit(10000).expect("Batch error");
+
 }
